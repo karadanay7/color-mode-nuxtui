56b813c2b1bee25d4ed94b16609cb4dc6bc60af9
Aysegul <karadan.ay7@gmail.com>
2024-09-22 19:14:47 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold mb-4">Welcome to My Color Mode APP</h1>
+            <p class="text-lg mb-6">We're glad to have you here. Explore and Enjoy!</p>
+            <UButton>Get Started!</UButton>
+        </div>
+
+    </div>
+</template>
+
